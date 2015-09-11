1448e4947b8df1a55d212c31302f4b17589100ab
Rurik Bugdanov <rurik.bugdanov@alsenet.com>
2015-09-11 23:41:00 +0700

    updated database structure

--- doxel.sql
+++ doxel.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 07, 2015 at 11:33 PM
+-- Generation Time: Sep 11, 2015 at 06:09 PM
 -- Server version: 10.0.20-MariaDB-0+deb8u1
 -- PHP Version: 5.6.12-0+deb8u1
 
@@ -31,11 +31,12 @@ USE `doxel`;
 DROP TABLE IF EXISTS `pictures`;
 CREATE TABLE IF NOT EXISTS `pictures` (
 `id` bigint(20) NOT NULL,
-  `user` int(11) DEFAULT NULL,
-  `created` timestamp(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `flags` bit(32) DEFAULT NULL,
-  `timestamp` timestamp(6) NULL DEFAULT NULL,
-  `segment` timestamp(6) NULL DEFAULT NULL,
+  `sha256` binary(32) NOT NULL,
+  `user` int(11) NOT NULL,
+  `created` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `flags` bit(32) NOT NULL,
+  `timestamp` timestamp(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000',
+  `segment` timestamp(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000',
   `lon` double DEFAULT NULL,
   `lat` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -48,11 +49,11 @@ CREATE TABLE IF NOT EXISTS `pictures` (
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `name` varchar(254) NOT NULL,
   `fingerprint` char(32) NOT NULL,
   `pass` varchar(32) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `ip` varchar(45) NOT NULL,
   `forwarded_for` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -65,7 +66,13 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Indexes for table `pictures`
 --
 ALTER TABLE `pictures`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `picture` (`id`), ADD KEY `timestamp` (`timestamp`);
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`), ADD KEY `timestamp` (timestamp), ADD KEY `user` (user);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -76,6 +83,11 @@ ALTER TABLE `pictures`
 --
 ALTER TABLE `pictures`
 MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
